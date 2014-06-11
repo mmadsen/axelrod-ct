8e72667f74eb4048804248f1404e64980592af52
Mark Madsen <mark@madsenlab.org>
2014-06-11 09:04:07 -0700

    need sudo to copy dreadnaut to /usr/local/bin

--- install-nauty.sh
+++ install-nauty.sh
@@ -4,5 +4,6 @@ set -ex
 cd /tmp
 wget --no-check-certificate http://pallini.di.uniroma1.it/nauty25r9.tar.gz -O /tmp/nauty25r9.tar.gz
 tar -xzvf nauty25r9.tar.gz
-cd nauty25r9 && ./configure && make && cp dreadnaut /usr/local/bin
+cd nauty25r9 && ./configure && make
+sudo cp dreadnaut /usr/local/bin
 
